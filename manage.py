e2de1d1a52f6331b3f60529a3d7a59c53e835ef6
riverfjs <riverfjs@gmail.com>
2021-05-04 15:34:59 +0800

    fix path

--- manage.py
+++ manage.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask import render_template
 from flask import url_for
 from flask import request
-
-app = Flask(__name__)
+import os
+app = Flask(__name__, template_folder=os.getcwd() + "/web", root_path=os.getcwd())
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,7 +12,7 @@ def index():
         b = request.form['right']
         c = int(a) + int(b)        
         return render_template('index.html', RESULT = str(c))
-    return render_template('web/index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
